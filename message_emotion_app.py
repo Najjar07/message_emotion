9ce3e26652f5b6b63704f9ebbb2db3f124273c29
Najari Umar Jibril <najariuj@gmail.com>
2025-12-15 20:51:29 +0000

    Add files via upload

---
+++ message_emotion_app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import joblib
+import streamlit as st
+
+
+model_path = r"C:\Users\LENOVO\Desktop\sk\message_model.pkl"
+vectorizer_path = r"C:\Users\LENOVO\Desktop\sk\message_vectorizer.pkl"
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
+
+st.title("message emotion app")
+message =st.text_input("write your message here: ")
+
+if st.button("emotion"):
+    sample_data = pd.DataFrame({
+    "message": [message]
+    })
+
+
+    sample_data['message'] = sample_data['message'].str.lower()
+
+    converted_sample_data = vectorizer.transform(sample_data)
+
+    make_recommendation = model.predict(converted_sample_data)
+
+    st.success(f"emotion: {make_recommendation[0]}")
